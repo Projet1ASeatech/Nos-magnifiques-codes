caffc2a0eb9556bcd5907e014c739fb9b9e3b6e5
Philemousse <82588802+Philemousse@users.noreply.github.com>
2021-04-21 16:26:48 +0200

    Update affichage.c

--- affichage.c
+++ affichage.c
@@ -1,5 +1,7 @@
+#include "fonction.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 
 int WinMain(int argc, char* argv[])
@@ -18,15 +20,18 @@ int WinMain(int argc, char* argv[])
     rendu=SDL_CreateRenderer(window,
                              -1, // driver
                              SDL_RENDERER_SOFTWARE);
-    int N[10][10] = {{10,10,10,10,10,10,10,10,10,10},{10,0,0,0,0,0,0,0,0,10},{10,0,0,0,0,0,0,0,0,10},{10,0,0,0,1,1,0,0,0,10},{10,0,0,1,1,1,1,0,0,10},{10,0,1,1,1,1,1,1,0,10},{10,0,0,1,0,0,1,0,0,10},{10,0,0,0,0,0,0,0,0,10},{10,0,0,0,0,0,0,0,0,10},{10,0,0,0,0,0,0,0,0,10},{10,0,0,0,0,0,0,0,0,10},{10,10,10,10,10,10,10,10,10,10}};
-    int M[8][9] = {{0,0,0,0,0,0,0,0,0},
-               {0,0,1,1,0,1,1,0,0},
-               {0,1,1,1,1,1,1,1,0},
-               {0,1,1,1,1,1,1,1,0},
-               {0,0,1,1,1,1,1,0,0},
-               {0,0,0,1,1,1,0,0,0},
-               {0,0,0,0,1,0,0,0,0},
-               {0,0,0,0,0,0,0,0,0}};
+
+    int M[9][9] = {{10,10,10,10,10,10,10,10,10},
+        {10,0,1,1,0,1,1,0,10},
+        {10,1,1,1,1,1,1,1,10},
+        {10,1,1,1,1,1,1,1,10},
+        {10,0,1,1,1,1,1,0,10},
+        {10,0,0,1,1,1,0,0,10},
+        {10,0,0,0,1,0,0,0,10},
+        {10,0,0,0,0,0,0,0,10},
+        {10,10,10,10,10,10,10,10,10}
+    };
+
     int hauteur_fenetre = sizeof M / sizeof M[0]-2;
     int largeur_fenetre = sizeof M[0] / sizeof(int)-2;
     int cote = 50;
@@ -39,67 +44,71 @@ int WinMain(int argc, char* argv[])
     rectangle.h = hauteur_fenetre*cote+2;
     SDL_RenderDrawRect(rendu, &rectangle);
 
+    int numite=1;
+    bool* running = true;
+    bool* modif;
+    int length = sizeof M[0];
 
-    for(int k=0;k<=hauteur_fenetre;k++)
+    while (running)
     {
-        for(int l=0;l<=largeur_fenetre;l++)
+        *modif=true;
+        while (modif)
         {
-            if (M[k][l]==1)
+            *modif=false;
+            change(M, numite, length, modif);
+            SDL_Delay(1000);
+            for(int k=0; k<=hauteur_fenetre; k++)
             {
-                SDL_SetRenderDrawColor(rendu, 255, 0, 0, SDL_ALPHA_OPAQUE);
-                SDL_Rect carre;
-                rectangle.x = 1+(l-1)*cote;
-                rectangle.y = 1+(k-1)*cote;
-                rectangle.w = cote;
-                rectangle.h = cote;
-                SDL_RenderDrawRect(rendu, &rectangle);
-                SDL_RenderFillRect(rendu, &rectangle);
+                for(int l=0; l<=largeur_fenetre; l++)
+                {
+                    if (M[k][l]==1)
+                    {
+                        SDL_SetRenderDrawColor(rendu, 255, 0, 0, SDL_ALPHA_OPAQUE);
+                        SDL_Rect carre;
+                        rectangle.x = 1+(l-1)*cote;
+                        rectangle.y = 1+(k-1)*cote;
+                        rectangle.w = cote;
+                        rectangle.h = cote;
+                        SDL_RenderDrawRect(rendu, &rectangle);
+                        SDL_RenderFillRect(rendu, &rectangle);
+                    }
+                    if (M[k][l]==0)
+                    {
+                        SDL_SetRenderDrawColor(rendu, 255, 255, 255, SDL_ALPHA_OPAQUE);
+                        SDL_Rect carre;
+                        rectangle.x = 1+(l-1)*cote;
+                        rectangle.y = 1+(k-1)*cote;
+                        rectangle.w = cote;
+                        rectangle.h = cote;
+                        SDL_RenderDrawRect(rendu, &rectangle);
+                    }
+                }
             }
-            if (M[k][l]==0)
-            {
-                SDL_SetRenderDrawColor(rendu, 255, 255, 255, SDL_ALPHA_OPAQUE);
-                SDL_Rect carre;
-                rectangle.x = 1+(l-1)*cote;
-                rectangle.y = 1+(k-1)*cote;
-                rectangle.w = cote;
-                rectangle.h = cote;
-                SDL_RenderDrawRect(rendu, &rectangle);
-            }
+            numite=-numite;
         }
-    }
-
-
-    SDL_RenderPresent(rendu);
-
-    SDL_bool programme_en_cours = SDL_TRUE;
-
-    while(programme_en_cours)
-    {
-
         SDL_Event event;
         while(SDL_PollEvent(&event))
         {
             if (event.type == SDL_QUIT )
             {
-                programme_en_cours = SDL_FALSE;
+                running = false;
                 printf("Bye  ");
             }
             if (event.type == SDL_KEYDOWN )
                 switch(event.key.keysym.sym)
                 {
                 case SDLK_ESCAPE :
-                    programme_en_cours = SDL_FALSE;
+                    running = false;
                 case SDLK_EXECUTE :
-                    //refresh
+                //refresh
                 default :
                     break;
                 }
         }
+        SDL_RenderPresent(rendu);
         SDL_Delay(16);
     }
     SDL_DestroyRenderer(rendu);
     SDL_DestroyWindow(window);
     SDL_Quit();
 }
-
-
