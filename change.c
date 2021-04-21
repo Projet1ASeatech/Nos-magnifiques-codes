b3cd6df0968ee830b804310e86afd4be13810abb
Philemousse <82588802+Philemousse@users.noreply.github.com>
2021-04-21 14:50:51 +0200

    Update change.c

--- change.c
+++ change.c
@@ -1,7 +1,7 @@
 void(int matrice[][], int numite, int length){
   for (int i = length(M[0]); i>0; i--){
     for (int j = length(M[0]); i>0; i--){
-      gravity(int matrice[][], int modif[], int numodif, Coordonnee immobiles[], int numimmo, int i, int j, int numite)
+      gravity(int matrice[][], int* modif, int i, int j, int numite)
     }
   }
 }
