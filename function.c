47a3c3f98ef19c5b3fd66c5fa5fdb2cb9a8f9758
helene-bnt <82588643+helene-bnt@users.noreply.github.com>
2021-04-21 14:01:08 +0200

    Update function.c
    effacement des atoi (ne servent à rien, on utilise directement des types int)

--- function.c
+++ function.c
@@ -1,6 +1,6 @@
 void gravity(char matrice[][], int modif[], int numodif, Coordonnee immobiles[], int numimmo, int i, int j, int numite){
   Coordonnee NC;
-  if (atoi(matrice[i+1][j]) < atoi(matrice[i][j])){                         //Test puis effectue si possible le déplacement vers le bas
+  if (matrice[i+1][j] < matrice[i][j]){                         //Test puis effectue si possible le déplacement vers le bas
     char temp = matrice[i+1][j];
     matrice[i+1][j] = matrice[i][j];
     matrice[i][j] = temp;
@@ -9,7 +9,7 @@ void gravity(char matrice[][], int modif[], int numodif, Coordonnee immobiles[],
     modif[numodif] = NC;                                                    //Mets à jour la matrice des modifiés
     numodif += 1;
   }
-  else if (atoi(matrice[i+1][j+numite]) < atoi(matrice[i][j])){             //Test puis effectue si possible le déplacement en diagonale
+  else if (matrice[i+1][j+numite] < matrice[i][j]){             //Test puis effectue si possible le déplacement en diagonale
     char temp = matrice[i+1][j+numite];
     matrice[i+1][j+numite] = matrice[i][j];
     matrice[i][j] = temp;
@@ -18,7 +18,7 @@ void gravity(char matrice[][], int modif[], int numodif, Coordonnee immobiles[],
     modif[numodif] = NC;                                                    //Mets à jour la matrice des modifiés
     numodif += 1;
   }
-  else if (atoi(matrice[i+1][j-numite]) < atoi(matrice[i][j])){             //Test puis effectue si possible le déplacement en diagonale dans l'autre direction
+  else if (matrice[i+1][j-numite] < matrice[i][j]){             //Test puis effectue si possible le déplacement en diagonale dans l'autre direction
     char temp = matrice[i+1][j-numite];
     matrice[i+1][j-numite] = matrice[i][j];
     matrice[i][j] = temp;
@@ -27,7 +27,7 @@ void gravity(char matrice[][], int modif[], int numodif, Coordonnee immobiles[],
     modif[numodif] = NC;                                                    //Mets à jour la matrice des modifiés
     numodif += 1;
   }
-  else if (atoi(matrice[i][j+numite]) < atoi(matrice[i][j])){               //Test puis effectue si possible le déplacement vers le côté
+  else if (matrice[i][j+numite] < matrice[i][j]){               //Test puis effectue si possible le déplacement vers le côté
     char temp = matrice[i][j+numite];
     matrice[i][j+numite] = matrice[i][j];
     matrice[i][j] = temp;
@@ -36,7 +36,7 @@ void gravity(char matrice[][], int modif[], int numodif, Coordonnee immobiles[],
     modif[numodif] = NC;                                                    //Mets à jour la matrice des modifiés
     numodif += 1;
   }
-  else if (atoi(matrice[i][j-numite]) < atoi(matrice[i][j])){               //Test puis effectue si possible le déplacement vers le côté dans l'autre direction
+  else if (matrice[i][j-numite] < matrice[i][j]){               //Test puis effectue si possible le déplacement vers le côté dans l'autre direction
     char temp = matrice[i][j-numite];
     matrice[i][j-numite] = matrice[i][j];
     matrice[i][j] = temp;
