9636dc1d55c024b2ffdd8bddd4af6ef4e2ad17ea
Philemousse <82588802+Philemousse@users.noreply.github.com>
2021-04-28 13:45:40 +0200

    Update function.h

--- function.h
+++ function.h
@@ -1,12 +1,13 @@
 #ifndef FONCTION_H
 #define FONCTION_H
+#define SIZE 9
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-void gravity(int matrice, &bool modif, int i, int j, int numite);
-void change(int matrice, int numite, int length, &bool modif);
-void affiche(int matrice);
+void gravity(int M[SIZE][SIZE], bool*, int, int, int);
+void change(int M[SIZE][SIZE], int, int, bool*);
+void affiche(int M[SIZE][SIZE]);
 
 #endif
