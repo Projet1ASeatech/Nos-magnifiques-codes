ed620518a4e94cae04a55ed97bcb311060fbfb21
helene-bnt <82588643+helene-bnt@users.noreply.github.com>
2021-04-28 15:29:21 +0200

    Update function.h
    ajout fonction matrice

--- function.h
+++ function.h
@@ -1,6 +1,7 @@
-#ifndef FUNCTION_H
-#define FUNCTION_H
-#define SIZE 15
+#ifndef FONCTION_H
+#define FONCTION_H
+#define WIDTH 9
+#define HEIGHT 9
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,5 +10,6 @@
 void gravity(int M[SIZE][SIZE], bool*, int, int, int);
 void change(int M[SIZE][SIZE], int, int, bool*);
 void affiche(int M[SIZE][SIZE]);
+int matrice(void); // initialise la matrice M
 
 #endif
