2faefc84d1261c95c403f807b20c1cf4ed2d19e9
Philemousse <82588802+Philemousse@users.noreply.github.com>
2021-04-28 17:16:18 +0200

    Update fonction.h

--- fonction.h
+++ fonction.h
@@ -1,14 +1,15 @@
 #ifndef FONCTION_H
 #define FONCTION_H
-#define WIDTH 15
-#define HEIGHT 17
+#define WIDTH 20
+#define HEIGHT 28
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
 void gravity(int M[HEIGHT][WIDTH], bool*, int, int, int);
-void change(int M[HEIGHT][WIDTH], int, int, bool*);
-int matrice(void); // initialise la matrice M
+void change(int M[HEIGHT][WIDTH], int, bool*);
+void matrice(int M[HEIGHT][WIDTH]); // initialise la matrice M
+void add_water(int M[HEIGHT][WIDTH], int, int, int, int);
 
 #endif
