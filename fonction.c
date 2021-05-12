1509d6bf8389f465796368f71acf82cced5e4cc7
Philemousse <82588802+Philemousse@users.noreply.github.com>
2021-05-12 15:51:24 +0200

    Update fonction.c

--- fonction.c
+++ fonction.c
@@ -14,23 +14,25 @@ void gravity(int M[HEIGHT][WIDTH], bool* modif, int i, int j, int numite)
 {
     if (M[i][j]==10 || M[i][j]==0){
     }
-    else if (M[i][j] == 1){
-        else if (M[i+1][j] < M[i][j])                           //Test puis effectue si possible le déplacement vers le bas
+    else if (M[i][j] == 1)
+    {                                                           //Vérifie la nature de la case
+        if (M[i+1][j] < M[i][j])                           //Test puis effectue si possible le déplacement vers le bas
         {
             char temp = M[i+1][j];
             M[i+1][j] = M[i][j];
             M[i][j] = temp;
             *modif = true;
         }
-        else if (M[i][j+numite] != 10){
-            if (M[i+1][j+numite] < M[i][j])                    //Test puis effectue si possible le déplacement en diagonale
+        else if (M[i][j+numite] != 10)
+        {                                                       //Vérifie qu'il n'y ait pas de mur du côté numite
+            if (M[i+1][j+numite] < M[i][j])                     //Test puis effectue si possible le déplacement en diagonale
             {
                 char temp = M[i+1][j+numite];
                 M[i+1][j+numite] = M[i][j];
                 M[i][j] = temp;
                 *modif = true;
             }
-            else if (M[i][j+numite] < M[i][j])                      //Test puis effectue si possible le déplacement vers le côté
+            else if (M[i][j+numite] < M[i][j])                  //Test puis effectue si possible le déplacement vers le côté
             {
                 char temp = M[i][j+numite];
                 M[i][j+numite] = M[i][j];
@@ -38,15 +40,57 @@ void gravity(int M[HEIGHT][WIDTH], bool* modif, int i, int j, int numite)
                 *modif = true;
             }
         }
-        else if (M[i][j-numite] != 10){
-            if (M[i+1][j-numite] < M[i][j])                    //Test puis effectue si possible le déplacement en diagonale dans l'autre direction
+        else if (M[i][j-numite] != 10)
+        {                                                       //Vérifie qu'il n'y ait pas de mur du côté -numite
+            if (M[i+1][j-numite] < M[i][j])                     //Test puis effectue si possible le déplacement en diagonale dans l'autre direction
             {
                 char temp = M[i+1][j-numite];
                 M[i+1][j-numite] = M[i][j];
                 M[i][j] = temp;
                 *modif = true;
             }
-            else if (M[i][j-numite] < M[i][j])                      //Test puis effectue si possible le déplacement vers le côté dans l'autre direction
+            else if (M[i][j-numite] < M[i][j])                  //Test puis effectue si possible le déplacement vers le côté dans l'autre direction
+            {
+                char temp = M[i][j-numite];
+                M[i][j-numite] = M[i][j];
+                M[i][j] = temp;
+                *modif = true;
+            }
+        }
+    }
+    else if (M[i][j] == 2){                                     //Vérifie la nature de la case
+        if (M[i-1][j] < M[i][j])                           //Test puis effectue si possible le déplacement vers le haut
+        {
+            char temp = M[i-1][j];
+            M[i-1][j] = M[i][j];
+            M[i][j] = temp;
+            *modif = true;
+        }
+        else if (M[i][j+numite] != 10){                         //Vérifie qu'il n'y ait pas de mur du côté numite
+            if (M[i-1][j+numite] < M[i][j])                     //Test puis effectue si possible le déplacement en diagonale
+            {
+                char temp = M[i+1][j+numite];
+                M[i-1][j+numite] = M[i][j];
+                M[i][j] = temp;
+                *modif = true;
+            }
+            else if (M[i][j+numite] < M[i][j])                  //Test puis effectue si possible le déplacement vers le côté
+            {
+                char temp = M[i][j+numite];
+                M[i][j+numite] = M[i][j];
+                M[i][j] = temp;
+                *modif = true;
+            }
+        }
+        else if (M[i][j-numite] != 10){                         //Vérifie qu'il n'y ait pas de mur du côté -numite
+            if (M[i-1][j-numite] < M[i][j])                     //Test puis effectue si possible le déplacement en diagonale dans l'autre direction
+            {
+                char temp = M[i+1][j-numite];
+                M[i-1][j-numite] = M[i][j];
+                M[i][j] = temp;
+                *modif = true;
+            }
+            else if (M[i][j-numite] < M[i][j])                  //Test puis effectue si possible le déplacement vers le côté dans l'autre direction
             {
                 char temp = M[i][j-numite];
                 M[i][j-numite] = M[i][j];
