d424376aadf1a906124fe1ccf392353ec4c5e3bb
Martial Guidez <82588663+Luxionos@users.noreply.github.com>
2021-06-02 16:24:38 +0200

    Update fonction.h

--- fonction.h
+++ fonction.h
@@ -8,11 +8,11 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void gravity(int M[HEIGHT][WIDTH], int, int, int);
-void change(int M[HEIGHT][WIDTH], int);
-void matrice(int M[HEIGHT][WIDTH]); // initialise la matrice M
-void add_blocks(int M[HEIGHT][WIDTH], int, int, int, int);
-void create_window(SDL_Renderer* rendu, int cote, SDL_Rect fenetre);
+void gravity(int M[HEIGHT][WIDTH], int, int, int);                                               // Gère les déplacements des différents blocs
+void change(int M[HEIGHT][WIDTH], int);                                                          // Actualise la matrice en appliquant la fonction gravity à chaque blocs
+void matrice(int M[HEIGHT][WIDTH]);                                                              // Initialise la matrice M
+void add_blocks(int M[HEIGHT][WIDTH], int, int, int, int);                                       // Change le type de bloc aux coordonnées x, y
+void create_window(SDL_Renderer* rendu, int cote, SDL_Rect fenetre);                             // 
 void update_affichage(int M[HEIGHT][WIDTH], SDL_Renderer* rendu, int cote);
 
 #endif
